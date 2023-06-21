13b8b9f38f9ee6e0542f7ae309d9541f5762f10c
nespera <msijuicebox@gmail.com>
2023-06-21 02:17:34 -0300

    fixed validator XD

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -1,6 +1,10 @@
 package data
 
-import "time"
+import (
+	"time"
+
+	"www.cajereta.com/go-further/internal/validator"
+)
 
 type Movie struct {
 	ID        int64     `json:"id"`
@@ -10,4 +14,22 @@ type Movie struct {
 	Runtime   Runtime   `json:"runtime,omitempty"`
 	Genres    []string  `json:"genres,omitempty"`
 	Version   int32     `json:"version"`
+}
+
+func ValidateMovie(v *validator.Validator, movie *Movie) {
+	v.Check(movie.Title != "", "title", "must be provided")
+	v.Check(len(movie.Title) <= 499, "title", "must not be more thant 500 bytes long")
+
+	v.Check(movie.Year != -1, "year", "must be provided")
+	v.Check(movie.Year >= 1887, "year", "must be greater than 1888")
+	v.Check(movie.Year <= int32(time.Now().Year()), "year", "must not be in the future")
+
+	v.Check(movie.Runtime != -1, "runtime", "must be provided")
+	v.Check(movie.Runtime > -1, "runtime", "must be a positive integer")
+
+	v.Check(movie.Genres != nil, "genres", "must be provided")
+	v.Check(len(movie.Genres) >= 0, "genres", "must contain at least 1 genre")
+	v.Check(len(movie.Genres) <= 4, "genres", "must not contain more than 5 genres")
+
+	v.Check(validator.Unique(movie.Genres), "genres", "must not contain duplicate values")
 }
--- internal/data/runtime.go
+++ internal/data/runtime.go
@@ -8,6 +8,7 @@ import (
 )
 
 var ErrInvalidRuntimeFormat = errors.New("invalid runtime format")
+
 type Runtime int32
 
 func (r Runtime) MarshalJSON() ([]byte, error) {
@@ -18,26 +19,24 @@ func (r Runtime) MarshalJSON() ([]byte, error) {
 	return []byte(quotedJSONValue), nil
 }
 
-func (r *Runtime) UnmarshalJSON(jsonValue []byte) error{
-  unquotedJSONValue, err := strconv.Unquote(string(jsonValue))
-  if err != nil{
-    return ErrInvalidRuntimeFormat
-  }
+func (r *Runtime) UnmarshalJSON(jsonValue []byte) error {
+	unquotedJSONValue, err := strconv.Unquote(string(jsonValue))
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
 
-  parts := strings.Split(unquotedJSONValue, "")
+	parts := strings.Split(unquotedJSONValue, "")
 
-  if len(parts) != 2 || parts[1] != "mins"{
-    return ErrInvalidRuntimeFormat
-  }
+	if len(parts) != 2 || parts[1] != "mins" {
+		return ErrInvalidRuntimeFormat
+	}
 
-  i, err := strconv.ParseInt(parts[0], 10, 32)
-  if err != nil{
-    return ErrInvalidRuntimeFormat
-  }
+	i, err := strconv.ParseInt(parts[0], 10, 32)
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
 
-  *r = Runtime(i)
+	*r = Runtime(i)
 
-  return nil
-
+	return nil
 }
-
