d43ba69f21e3c9f73b727393052c78c144dd62b1
nespera <msijuicebox@gmail.com>
2023-06-21 00:31:16 -0300

    added errors and json encoding

--- internal/data/runtime.go
+++ internal/data/runtime.go
@@ -7,11 +7,10 @@ import (
 
 type Runtime int32
 
-func (r Runtime) MarshalJSON() ([]byte, error){
-  jsonValue := fmt.Sprintf("%d mins", r)
+func (r Runtime) MarshalJSON() ([]byte, error) {
+	jsonValue := fmt.Sprintf("%d mins", r)
 
-  quotedJSONValue := strconv.Quote(jsonValue)
+	quotedJSONValue := strconv.Quote(jsonValue)
 
-  return []byte(quotedJSONValue), nil
-
+	return []byte(quotedJSONValue), nil
 }
