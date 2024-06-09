c7fad7e7c0df1fdc465755c2c90c9ac668277579
Emanuel Gomez <msijuicebox@gmail.com>
2024-06-09 20:40:05 -0300

    json as string

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -48,9 +48,3 @@ func main() {
 	logger.Error(err.Error())
 	os.Exit(1)
 }
-
-func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "status: available")
-	fmt.Fprintf(w, "enviroment: %s\n", app.config.env)
-	fmt.Fprintf(w, "version: %s\n", version)
-}
