386c824feb606a4c884e4cd80b075f1d94beb0b9
Emanuel Gomez <msijuicebox@gmail.com>
2024-06-15 08:09:36 -0300

    include metadata in json response

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -34,7 +34,7 @@ func (app *application) listMoviesHandler(w http.ResponseWriter, r *http.Request
 		return
 	}
 
-	movies, err := app.models.Movies.GetAll(input.Title, input.Genres, input.Filters)
+	movies, metadata, err := app.models.Movies.GetAll(input.Title, input.Genres, input.Filters)
 
 	if err != nil {
 		app.serverErrorResponse(w, r, err)
@@ -42,7 +42,7 @@ func (app *application) listMoviesHandler(w http.ResponseWriter, r *http.Request
 
 	}
 
-	err = app.writeJSON(w, http.StatusOK, envelope{"movies": movies}, nil)
+	err = app.writeJSON(w, http.StatusOK, envelope{"movies": movies, "metadata": metadata}, nil)
 
 	if err != nil {
 		app.serverErrorResponse(w, r, err)
