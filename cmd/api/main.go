b0a195a0fa96d8b83c5bebef879ddda592e14291
Emanuel Gomez <msijuicebox@gmail.com>
2024-06-18 05:21:18 -0300

    can now set the variables for rate limiting

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -27,6 +27,11 @@ type config struct {
 		maxIdleConns int
 		maxIdleTime  string
 	}
+	limiter struct {
+		rps     float64
+		burst   int
+		enabled bool
+	}
 }
 
 type application struct {
@@ -45,12 +50,20 @@ func main() {
 	postgresURL := os.Getenv("POSTGRES_URL")
 	var cfg config
 
+	//Ports, env, database
 	flag.IntVar(&cfg.port, "port", 4000, "API server port")
 	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
 	flag.StringVar(&cfg.db.dsn, "db-dsn", postgresURL, "PostgreSQL DSN")
+
+	//Database connection pool
 	flag.IntVar(&cfg.db.maxOpenConns, "db-max-open-conns", 25, "PostgreSQL max open connections")
 	flag.IntVar(&cfg.db.maxIdleConns, "db-max-idle-conns", 25, "PostgreSQL max idle connnections")
 	flag.StringVar(&cfg.db.maxIdleTime, "db-max-idle-time", "15m", "PostgreSQL max connection idle time")
+
+	//Rate limiter
+	flag.Float64Var(&cfg.limiter.rps, "limiter-rps", 5, "Rate limiter maximum request per second")
+	flag.IntVar(&cfg.limiter.burst, "limiter-bust", 10, "Rate limiter maximum burst")
+	flag.BoolVar(&cfg.limiter.enabled, "limiter-enabled", true, "Rate limiter enabled")
 
 	flag.Parse()
 
@@ -83,7 +96,7 @@ func main() {
 		"addr": srv.Addr,
 		"env":  cfg.env,
 	})
-	
+
 	err = srv.ListenAndServe()
 	logger.PrintFatal(err, nil)
 }
